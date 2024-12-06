60ab12fdae52e6a8a2101e9b201e3d62b5452d22
Alex Dubuisson <adubuisson@normanmax.com>
2024-12-06 09:20:09 -0500

    edit app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,732 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BackTestingWizard } from 'back-testing-react'
 function App() {
+
+    function payoutTable_74_119() {
+        const retVal = [
+            {
+                "category": 74,
+                "payout": 0.05
+            },
+            {
+                "category": 75,
+                "payout": 0.055
+            },
+            {
+                "category": 76,
+                "payout": 0.06
+            },
+            {
+                "category": 77,
+                "payout": 0.065
+            },
+            {
+                "category": 78,
+                "payout": 0.07
+            },
+            {
+                "category": 79,
+                "payout": 0.075
+            },
+            {
+                "category": 80,
+                "payout": 0.08
+            },
+            {
+                "category": 81,
+                "payout": 0.085
+            },
+            {
+                "category": 82,
+                "payout": 0.09
+            },
+            {
+                "category": 83,
+                "payout": 0.095
+            },
+            {
+                "category": 84,
+                "payout": 0.1
+            },
+            {
+                "category": 85,
+                "payout": 0.11
+            },
+            {
+                "category": 86,
+                "payout": 0.12
+            },
+            {
+                "category": 87,
+                "payout": 0.13
+            },
+            {
+                "category": 88,
+                "payout": 0.14
+            },
+            {
+                "category": 89,
+                "payout": 0.15
+            },
+            {
+                "category": 90,
+                "payout": 0.16
+            },
+            {
+                "category": 91,
+                "payout": 0.17
+            },
+            {
+                "category": 92,
+                "payout": 0.18
+            },
+            {
+                "category": 93,
+                "payout": 0.19
+            },
+            {
+                "category": 94,
+                "payout": 0.2
+            },
+            {
+                "category": 95,
+                "payout": 0.22
+            },
+            {
+                "category": 96,
+                "payout": 0.24
+            },
+            {
+                "category": 97,
+                "payout": 0.26
+            },
+            {
+                "category": 98,
+                "payout": 0.28
+            },
+            {
+                "category": 99,
+                "payout": 0.3
+            },
+            {
+                "category": 100,
+                "payout": 0.32
+            },
+            {
+                "category": 101,
+                "payout": 0.34
+            },
+            {
+                "category": 102,
+                "payout": 0.36
+            },
+            {
+                "category": 103,
+                "payout": 0.38
+            },
+            {
+                "category": 104,
+                "payout": 0.4
+            },
+            {
+                "category": 105,
+                "payout": 0.42
+            },
+            {
+                "category": 106,
+                "payout": 0.44
+            },
+            {
+                "category": 107,
+                "payout": 0.46
+            },
+            {
+                "category": 108,
+                "payout": 0.48
+            },
+            {
+                "category": 109,
+                "payout": 0.5
+            },
+            {
+                "category": 110,
+                "payout": 0.55
+            },
+            {
+                "category": 111,
+                "payout": 0.6
+            },
+            {
+                "category": 112,
+                "payout": 0.65
+            },
+            {
+                "category": 113,
+                "payout": 0.7
+            },
+            {
+                "category": 114,
+                "payout": 0.75
+            },
+            {
+                "category": 115,
+                "payout": 0.8
+            },
+            {
+                "category": 116,
+                "payout": 0.85
+            },
+            {
+                "category": 117,
+                "payout": 0.9
+            },
+            {
+                "category": 118,
+                "payout": 0.95
+            },
+            {
+                "category": 119,
+                "payout": 1
+            }
+        ]
+        return retVal
+    }
+
+    function payoutTable_65_110() {
+        const retVal = [
+            {
+                "category": 65,
+                "payout": 0.01
+            },
+            {
+                "category": 66,
+                "payout": 0.015
+            },
+            {
+                "category": 67,
+                "payout": 0.02
+            },
+            {
+                "category": 68,
+                "payout": 0.025
+            },
+            {
+                "category": 69,
+                "payout": 0.03
+            },
+            {
+                "category": 70,
+                "payout": 0.035
+            },
+            {
+                "category": 71,
+                "payout": 0.04
+            },
+            {
+                "category": 72,
+                "payout": 0.045
+            },
+            {
+                "category": 73,
+                "payout": 0.05
+            },
+            {
+                "category": 74,
+                "payout": 0.055
+            },
+            {
+                "category": 75,
+                "payout": 0.06
+            },
+            {
+                "category": 76,
+                "payout": 0.07
+            },
+            {
+                "category": 77,
+                "payout": 0.08
+            },
+            {
+                "category": 78,
+                "payout": 0.09
+            },
+            {
+                "category": 79,
+                "payout": 0.1
+            },
+            {
+                "category": 80,
+                "payout": 0.11
+            },
+            {
+                "category": 81,
+                "payout": 0.12
+            },
+            {
+                "category": 82,
+                "payout": 0.13
+            },
+            {
+                "category": 83,
+                "payout": 0.14
+            },
+            {
+                "category": 84,
+                "payout": 0.15
+            },
+            {
+                "category": 85,
+                "payout": 0.16
+            },
+            {
+                "category": 86,
+                "payout": 0.17
+            },
+            {
+                "category": 87,
+                "payout": 0.18
+            },
+            {
+                "category": 88,
+                "payout": 0.19
+            },
+            {
+                "category": 89,
+                "payout": 0.2
+            },
+            {
+                "category": 90,
+                "payout": 0.22
+            },
+            {
+                "category": 91,
+                "payout": 0.24
+            },
+            {
+                "category": 92,
+                "payout": 0.26
+            },
+            {
+                "category": 93,
+                "payout": 0.28
+            },
+            {
+                "category": 94,
+                "payout": 0.3
+            },
+            {
+                "category": 95,
+                "payout": 0.32
+            },
+            {
+                "category": 96,
+                "payout": 0.34
+            },
+            {
+                "category": 97,
+                "payout": 0.36
+            },
+            {
+                "category": 98,
+                "payout": 0.38
+            },
+            {
+                "category": 99,
+                "payout": 0.4
+            },
+            {
+                "category": 100,
+                "payout": 0.42
+            },
+            {
+                "category": 101,
+                "payout": 0.44
+            },
+            {
+                "category": 102,
+                "payout": 0.46
+            },
+            {
+                "category": 103,
+                "payout": 0.48
+            },
+            {
+                "category": 104,
+                "payout": 0.5
+            },
+            {
+                "category": 105,
+                "payout": 0.55
+            },
+            {
+                "category": 106,
+                "payout": 0.6
+            },
+            {
+                "category": 107,
+                "payout": 0.7
+            },
+            {
+                "category": 108,
+                "payout": 0.8
+            },
+            {
+                "category": 109,
+                "payout": 0.9
+            },
+            {
+                "category": 110,
+                "payout": 1.0
+            }
+        ]
+        return retVal
+    }
+
+    function payoutTable_65_130() {
+        const retVal = [
+            {
+                "category": 65,
+                "payout": 0.01
+            },
+            {
+                "category": 66,
+                "payout": 0.015
+            },
+            {
+                "category": 67,
+                "payout": 0.02
+            },
+            {
+                "category": 68,
+                "payout": 0.025
+            },
+            {
+                "category": 69,
+                "payout": 0.03
+            },
+            {
+                "category": 70,
+                "payout": 0.035
+            },
+            {
+                "category": 71,
+                "payout": 0.04
+            },
+            {
+                "category": 72,
+                "payout": 0.045
+            },
+            {
+                "category": 73,
+                "payout": 0.05
+            },
+            {
+                "category": 74,
+                "payout": 0.055
+            },
+            {
+                "category": 75,
+                "payout": 0.06
+            },
+            {
+                "category": 76,
+                "payout": 0.065
+            },
+            {
+                "category": 77,
+                "payout": 0.07
+            },
+            {
+                "category": 78,
+                "payout": 0.075
+            },
+            {
+                "category": 79,
+                "payout": 0.08
+            },
+            {
+                "category": 80,
+                "payout": 0.085
+            },
+            {
+                "category": 81,
+                "payout": 0.09
+            },
+            {
+                "category": 82,
+                "payout": 0.095
+            },
+            {
+                "category": 83,
+                "payout": 0.1
+            },
+            {
+                "category": 84,
+                "payout": 0.11
+            },
+            {
+                "category": 85,
+                "payout": 0.12
+            },
+            {
+                "category": 86,
+                "payout": 0.13
+            },
+            {
+                "category": 87,
+                "payout": 0.14
+            },
+            {
+                "category": 88,
+                "payout": 0.15
+            },
+            {
+                "category": 89,
+                "payout": 0.16
+            },
+            {
+                "category": 90,
+                "payout": 0.17
+            },
+            {
+                "category": 91,
+                "payout": 0.18
+            },
+            {
+                "category": 92,
+                "payout": 0.19
+            },
+            {
+                "category": 93,
+                "payout": 0.2
+            },
+            {
+                "category": 94,
+                "payout": 0.22
+            },
+            {
+                "category": 95,
+                "payout": 0.24
+            },
+            {
+                "category": 96,
+                "payout": 0.26
+            },
+            {
+                "category": 97,
+                "payout": 0.28
+            },
+            {
+                "category": 98,
+                "payout": 0.3
+            },
+            {
+                "category": 99,
+                "payout": 0.32
+            },
+            {
+                "category": 100,
+                "payout": 0.34
+            },
+            {
+                "category": 101,
+                "payout": 0.36
+            },
+            {
+                "category": 102,
+                "payout": 0.38
+            },
+            {
+                "category": 103,
+                "payout": 0.4
+            },
+            {
+                "category": 104,
+                "payout": 0.42
+            },
+            {
+                "category": 105,
+                "payout": 0.44
+            },
+            {
+                "category": 106,
+                "payout": 0.46
+            },
+            {
+                "category": 107,
+                "payout": 0.48
+            },
+            {
+                "category": 108,
+                "payout": 0.5
+            },
+            {
+                "category": 109,
+                "payout": 0.52
+            },
+            {
+                "category": 110,
+                "payout": 0.54
+            },
+            {
+                "category": 111,
+                "payout": 0.56
+            },
+            {
+                "category": 112,
+                "payout": 0.58
+            },
+            {
+                "category": 113,
+                "payout": 0.6
+            },
+            {
+                "category": 114,
+                "payout": 0.62
+            },
+            {
+                "category": 115,
+                "payout": 0.64
+            },
+            {
+                "category": 116,
+                "payout": 0.66
+            },
+            {
+                "category": 117,
+                "payout": 0.68
+            },
+            {
+                "category": 118,
+                "payout": 0.7
+            },
+            {
+                "category": 119,
+                "payout": 0.72
+            },
+            {
+                "category": 120,
+                "payout": 0.74
+            },
+            {
+                "category": 121,
+                "payout": 0.76
+            },
+            {
+                "category": 122,
+                "payout": 0.78
+            },
+            {
+                "category": 123,
+                "payout": 0.8
+            },
+            {
+                "category": 124,
+                "payout": 0.82
+            },
+            {
+                "category": 125,
+                "payout": 0.84
+            },
+            {
+                "category": 126,
+                "payout": 0.86
+            },
+            {
+                "category": 127,
+                "payout": 0.88
+            },
+            {
+                "category": 128,
+                "payout": 0.9
+            },
+            {
+                "category": 129,
+                "payout": 0.95
+            },
+            {
+                "category": 130,
+                "payout": 1.0
+            }
+        ]
+        return retVal
+    }
+
+    function payoutTableCiac_10_25_50_75_100() {
+        const retVal = [
+            {
+                "category": 1,
+                "payout": 0.1
+            },
+            {
+                "category": 2,
+                "payout": 0.25
+            },
+            {
+                "category": 3,
+                "payout": 0.5
+            },
+            {
+                "category": 4,
+                "payout": 0.75
+            },
+            {
+                "category": 5,
+                "payout": 1
+            }
+        ]
+        return retVal;
+    }
+
+    function payoutTableCiac_30_60_100_100_100() {
+        const retVal = [
+            {
+                "category": 1,
+                "payout": 0.13
+            },
+            {
+                "category": 2,
+                "payout": 0.6
+            },
+            {
+                "category": 3,
+                "payout": 1
+            },
+            {
+                "category": 4,
+                "payout": 1
+            },
+            {
+                "category": 5,
+                "payout": 1
+            }
+        ]
+        return retVal;
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{height:'100vh',width:'100vw'}}>
+      <BackTestingWizard 
+        mapAccessToken="" 
+        apiAccessToken=""
+        env='uat'
+        selectedProxyPayoutOptionIndex={0}
+        proxyPayoutOptions={[
+            {key:'74 - 119 Standard Payout', value:payoutTable_74_119()},
+            {key:'65 - 110 Residential Payout', value: payoutTable_65_110()},
+            {key:'65 - 130 Residential Payout', value: payoutTable_65_130()}
+        ]}
+        selectedAnemometerPayoutOptionIndex={0}
+        anemometerPayoutOptions={[
+            {key:'74 - 119 Standard Payout', value:payoutTable_74_119()},
+            {key:'65 - 110 Residential Payout', value: payoutTable_65_110()},
+            {key:'65 - 130 Residential Payout', value: payoutTable_65_130()}
+        ]}
+        selectedCIACPayoutOptionIndex={0}
+        ciacPayoutOptions={[
+            {key:'CIAC 10/25/50/75/100', value:payoutTableCiac_10_25_50_75_100()},
+            {key:'CIAC 30/60/100/100/100', value: payoutTableCiac_30_60_100_100_100()}
+        ]}
+      />
     </div>
   );
 }
